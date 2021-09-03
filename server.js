5d5da9e69c0dcc647d8d8009a8e3c31044a0eae3
gjendi-kodnet <developer3@kodnet.no>
2021-09-03 13:40:10 +0200

    Creating basic routes for bootcamps

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+//Route files
+const bootcamps = require("./routes/bootcamps");
+//Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-const PORT = process.env.PORT;
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on ${PORT}`));
