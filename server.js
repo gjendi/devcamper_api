f2da94eafaf0ab5c264e66148b7e54732182176b
gjendi-kodnet <developer3@kodnet.no>
2021-09-03 11:52:39 +0200

    test develop branch

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, console.log(`Server running on ${PORT}`));
