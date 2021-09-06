ddcd873cba230aaca8de2143f2a027f162bdd200
gjendi-kodnet <developer3@kodnet.no>
2021-09-06 10:38:44 +0200

    Connected to MongoDb and handling unHandled rejection to crash the app

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDb = require("./config/db");
 //Route files
 const bootcamps = require("./routes/bootcamps");
+
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+//Connection to db
+connectDb();
 
 const app = express();
 
@@ -18,4 +23,10 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running on ${PORT}`));
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
