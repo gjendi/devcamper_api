54640b6a9c35ca3da789959656744f403f3b3604
gjendi-kodnet <developer3@kodnet.no>
2021-09-06 12:04:53 +0200

    Make POST crud work with some created data and addin body parser on server

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require("../models/Bootcamp");
+
 exports.getBootcamps = (req, res, next) => {
   res.status(200).json({ success: true, msg: "Show all bootcamps" });
 };
@@ -6,8 +8,16 @@ exports.getBootcamp = (req, res, next) => {
     .status(200)
     .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
 };
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Create a bootcamp" });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 exports.updateBootcamp = (req, res, next) => {
   res
