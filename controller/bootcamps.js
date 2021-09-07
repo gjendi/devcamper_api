4a4df8c19476f70b8a6cc9b3fce80e00670c171b
gjendi-kodnet <developer3@kodnet.no>
2021-09-07 15:44:12 +0200

    Implementin sorting on getBootcamps

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -5,17 +5,40 @@ const Bootcamp = require("../models/Bootcamp");
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
-  let queryStr = JSON.stringify(req.query);
+  //Copy req.query
+  const reqQuery = { ...req.query };
+  //Fields to exclude(to not match on filtering)
+  const removeFields = ["select", "sort"];
+  //Loop over removeFields and delete them from req query
+  removeFields.forEach((param) => delete reqQuery[param]);
+  //Create query string
+  let queryStr = JSON.stringify(reqQuery);
+  //Create operators ($gt,$gte etc)
   queryStr = queryStr.replace(
     /\b(gt|gte|lt|lte|in)\b/g,
     (match) => `$${match}`
   );
+  //Finding resources
   query = Bootcamp.find(JSON.parse(queryStr));
+  //Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+  //Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+  //Executing query
   const bootcamps = await query;
   res
     .status(200)
     .json({ success: true, count: bootcamps.length, data: bootcamps });
 });
+
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id);
   if (!bootcamp) {
