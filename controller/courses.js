c07ecb0050f65a04e8c24e60771cebb36f18be97
gjendi-kodnet <developer3@kodnet.no>
2021-09-08 15:56:55 +0200

    Implementing update course

--- controller/courses.js
+++ controller/courses.js
@@ -23,11 +23,13 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
 
 exports.getCourse = asyncHandler(async (req, res, next) => {
   const course = await Course.findById(req.params.id).populate({
-    path: 'bootcamp',
-    select: 'name description'
+    path: "bootcamp",
+    select: "name description",
   });
-  if(!course){
-    return next(new ErrorResponse(`No course were found with id ${req.params.id}`,404))
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course were found with id ${req.params.id}`, 404)
+    );
   }
   res.status(200).json({
     success: true,
@@ -38,12 +40,38 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
-    
-  if(!bootcamp){
-    return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`,404))
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `No bootcamp with the id of ${req.params.bootcampId}`,
+        404
+      )
+    );
   }
 
-  const course = await Course.create(req.body)
+  const course = await Course.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({
     success: true,
--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getCourses, getCourse, addCourse } = require("../controller/courses");
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+} = require("../controller/courses");
 
 const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getCourses).post(addCourse);
-router.route("/:id").get(getCourse);
+router.route("/:id").get(getCourse).put(updateCourse);
 
 module.exports = router;
