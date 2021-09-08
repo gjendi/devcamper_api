b74eb4a0a38f797e605e80b5394d446c36127255
gjendi-kodnet <developer3@kodnet.no>
2021-09-08 11:12:54 +0200

    Implementing Course model /importing to db seeder

--- seeder.js
+++ seeder.js
@@ -6,6 +6,7 @@ dotenv.config({ path: "./config/config.env" });
 
 //Load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 //Connect to db
 mongoose.connect(process.env.MONGO_URI, {
@@ -17,11 +18,15 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
 
 //Import into db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data imported to db");
     process.exit();
   } catch (error) {
@@ -33,6 +38,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data deleted from db");
     process.exit();
   } catch (error) {
@@ -42,6 +48,6 @@ const deleteData = async () => {
 
 if (process.argv[2] === "-i") {
   importData();
-} else if (process.argv[2] === "-d"){
+} else if (process.argv[2] === "-d") {
   deleteData();
 }
