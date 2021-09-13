7e5c0ff4512d721ac9ccbcfb20b392b8edaaee19
gjendi-kodnet <developer3@kodnet.no>
2021-09-13 15:21:36 +0200

    Bootcamp and user relationship implemented

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -18,6 +18,19 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: bootcamp });
 });
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  //Add user to req.body
+  req.body.user = req.user.id;
+  //check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+  //if the user is not admin can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== "admin") {
+    return next(
+      ErrorResponse(
+        `The user with id ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
   const bootcamp = await Bootcamp.create(req.body);
   res.status(201).json({
     success: true,
--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({ path: "./config/config.env" });
 //Load models
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 //Connect to db
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,12 +22,16 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 
 //Import into db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log("Data imported to db");
     process.exit();
   } catch (error) {
@@ -39,6 +44,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log("Data deleted from db");
     process.exit();
   } catch (error) {
